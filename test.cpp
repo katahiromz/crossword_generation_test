b4796751ef7740544f2a6f5e3b462ee24abe357a
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2021-11-13 08:51:44 +0900

    add dict

--- test.cpp
+++ test.cpp
@@ -51,6 +51,35 @@ std::unordered_set<std::string> s_words = {
 "WORK",
 };
 
+template <typename T_CHAR>
+inline void mstr_trim(std::basic_string<T_CHAR>& str, const T_CHAR *spaces)
+{
+    typedef std::basic_string<T_CHAR> string_type;
+    size_t i = str.find_first_not_of(spaces);
+    size_t j = str.find_last_not_of(spaces);
+    if ((i == string_type::npos) || (j == string_type::npos))
+    {
+        str.clear();
+    }
+    else
+    {
+        str = str.substr(i, j - i + 1);
+    }
+}
+
+bool load_dict(const char *filename, std::unordered_set<std::string>& dict) {
+    if (FILE *fp = fopen(filename, "r")) {
+        char buf[256];
+        while (fgets(buf, 256, fp)) {
+            std::string str = buf;
+            mstr_trim(str, " \t\r\n");
+            dict.insert(str);
+        }
+        return true;
+    }
+    return false;
+}
+
 int main(int argc, char **argv) {
     std::srand(::GetTickCount() ^ ::GetCurrentThreadId());
 
